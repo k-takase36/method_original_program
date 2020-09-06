bdf52a823ab713e71fdcdd9a70117ae1b713a50c
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-09-07 06:48:05 +0900

    need_to_decide

--- method_original.rb
+++ method_original.rb
@@ -1,4 +1,11 @@
 require "pry"
 
+CHILD_NAME = "ミスちゃん"
+ADULT_NAME = "ライトさん"
 
 words = Word.import(path: "words.csv")
+child = Child.new(name: CHILD_NAME, items: items)
+adult = Adult.new(name: ADULT_NAME, items: items)
+
+child.speak
+adult.speak
