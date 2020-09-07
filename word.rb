e44847d894871e01307c53ab6b9949b547f038dc
takaseko <yasaisekatu0306@gmail.com>
2020-09-08 05:51:55 +0900

    wordクラス、CSV読み込み作成

--- word.rb
+++ word.rb
@@ -1,3 +1,16 @@
+require "csv"
 class Word
+  def initialize(mistake:, right:)
+    @mistake= mistake
+    @right = right
+  end
 
+  def self.import(path:)
+    CSV.read(path, headers: true).map do |row|
+      Word.new(
+        mistake: row["mistake"],
+        right: row["right"],
+      )
+    end
+  end
 end
