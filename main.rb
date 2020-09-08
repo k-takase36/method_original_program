8649ee195d74932ddbaee52144fd320b25e60d2b
takaseko <yasaisekatu0306@gmail.com>
2020-09-09 07:30:32 +0900

    childファイル、クラスの作成

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ require_relative "child"
 
 CHILD_NAME = "ミスちゃん"
 ADULT_NAME = "ライトさん"
+
 words = Word.import(path: "words.csv")
 child = Child.new(name: CHILD_NAME, words: words)
 adult = Adult.new(name: ADULT_NAME, words: words)
