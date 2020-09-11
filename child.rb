0de46b828b9a6dec9819844c0846dc395f5cb5ef
takaseko <yasaisekatu0306@gmail.com>
2020-09-12 07:46:29 +0900

    child_speak_add

--- child.rb
+++ child.rb
@@ -3,10 +3,14 @@ class Child
     @name = name
     @words = words
   end
-  
+
   def speak
+    puts "#{@name}の言葉はこれだよ！"
+    puts "どこが違うの？"
+    puts "-" * 30
     @words.each.with_index(1) do |word, i|
       puts "#{i}. #{word.name}: #{word.words}"
     end
+    puts "-" * 30
   end
 end
